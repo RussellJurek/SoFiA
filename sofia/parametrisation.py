bf9f5ed644ce2e360d6c703c8adc494af98e9277
ngiese <giese@astro.rug.nl>
2014-02-07 00:31:37 +0100

    Create parametrisation.py
    Separate module/function that calls the parametrization and returns the input parameter array(s) with the new parameters appended.

---
+++ sofia/parametrisation.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+from sofia import parametrizer2
+import numpy as np
+
+
+def parametrise(cube,header,mask,objects,cathead,catformt,Parameters):
+  
+  cathead=np.array(cathead)
+  initcatalog=parametrizer2.PySourceCatalog()
+  for obj in objects:
+    newSource=parametrizer2.PySource()
+    newSource.setSourceID(obj[cathead=='ID'])
+    newMeasurement=parametrizer2.PyMeasurement()
+    newMeasurement.set("X",obj[cathead=='Xg'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("Y",obj[cathead=='Yg'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("Z",obj[cathead=='Zg'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_X_MIN",obj[cathead=='Xmin'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_X_MAX",obj[cathead=='Xmax'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_Y_MIN",obj[cathead=='Ymin'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_Y_MAX",obj[cathead=='Ymax'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_Z_MIN",obj[cathead=='Zmin'],0.,"")
+    newSource.setParameter(newMeasurement)
+    newMeasurement.set("BBOX_Z_MAX",obj[cathead=='Zmax'],0.,"")
+    newSource.setParameter(newMeasurement)
+    initcatalog.insert(newSource)
+
+  moduleParametrizer = parametrizer2.PyModuleParametrisation()
+  cube = cube.astype("<f4")
+  mask = mask.astype("<i2")
+  moduleParametrizer.setFlags(Parameters['parameters']['optimiseMask'], Parameters['parameters']['fitBusyFunction'])
+  moduleParametrizer.run(cube, mask, initcatalog, header)
+  results = moduleParametrizer.getCatalog()
+
+
+  # append the results to the objects array or reset
+  replParam = ['BBOX_X_MAX','BBOX_X_MIN','BBOX_Y_MAX','BBOX_Y_MIN','BBOX_Z_MAX','BBOX_Z_MIN','F_PEAK','F_TOT','ID','X','Y','Z']
+  origParam = ['Xmax','Xmin','Ymax','Ymin','Zmax','Zmin','Fmax','Ftot','ID','Xm','Ym','Zm']
+  d = results.getSourcesDict()
+  pars = d[d.keys()[0]].getParametersDict()
+  cathead = list(cathead)
+  catformt = list(catformt)
+  for i in sorted(pars):
+    if i not in replParam:
+      cathead.append(i)
+      catformt.append('%12.3f')
+  objects = objects.tolist()
+  for i in d:
+    source_dict = d[i].getParametersDict()
+    # check the source index
+    index = int(source_dict['ID'].getValue())
+    #print index
+    for j in sorted(source_dict):
+      if j in replParam:
+        objects[index-1][cathead.index(origParam[replParam.index(j)])] = source_dict[j].getValue()
+      else:
+        objects[index-1].append(source_dict[j].getValue())
+
+  objects = np.array(objects)
+  cathead = np.array(cathead)
+  catformt = np.array(catformt)
+  print 'The parameterisation has completed'
+  print
+  
+  #print objects.shape,cathead.shape
+  return objects,cathead,catformt
