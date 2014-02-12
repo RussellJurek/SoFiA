b48578eb74f1aa1ede308b595500afba061d17fb
ngiese <giese@astro.rug.nl>
2014-02-12 14:36:03 +0100

    Update store_xml.py
    Added function to store the full information from the source finding (in addition to the results from the parametrization). Additionally, it now accepts a list of parameters to write if the user does not wish to store all parameters. The old output function can be deleted if the new one is tested successfully.

--- sofia/store_xml.py
+++ sofia/store_xml.py
@@ -65,4 +65,52 @@ def make_xml(objects,outname):
 
 
 
+def make_xml_from_array(objects,cathead,catfmt,store_pars,outname):
+	print 'Store the results to xml file: ', outname
+	top = Element('VOTABLE')
+	resource = SubElement(top, 'RESOURCE', name='SoFiA catalogue')
+	description = SubElement(resource, 'DESCRIPTION')
+	description.text='Output catalogue from SoFiA'
+	coocys = SubElement(resource, 'COOSYS', ID='J2000')
+	table = SubElement(resource, 'TABLE', ID='sofia_cat', name='sofia_cat')
+	description = SubElement(table, 'DESCRIPTION')
+	description.text='Output catalogue from SoFiA'
+	
+	# write the parameters in fields:
+	if store_pars == ['*']:
+		for i in cathead:
+			field = SubElement(table, 'FIELD', name=i, datatype='float', unit='-')
+	else:
+		for par in store_pars:
+			index = list(cathead).index(par)
+			field = SubElement(table, 'FIELD', name=cathead[index], datatype='float', unit='-')
+	
+	# write the data	
+	data = SubElement(table, 'DATA')
+	tabledata = SubElement(data, 'TABLEDATA')
+
+	if store_pars == ['*']:
+		for obj in objects:
+			tr = SubElement(tabledata,'TR')
+			for i in range(0,len(obj)):
+				td = SubElement(tr,'TD')
+				td.text = (catfmt[i]%obj[i]).strip()
+			  
+			
+	else:	
+		for obj in objects:
+			tr = SubElement(tabledata,'TR')
+			for par in store_pars:
+				td = SubElement(tr,'TD')
+				index = list(cathead).index(par)
+				td.text = (catfmt[index]%obj[index]).strip()
+				
+
+
+	f1 = open(outname, 'w+')
+	f1.write(prettify(top))
+	f1.close
+
+	return
+
 
