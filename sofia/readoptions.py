022f199a1336570437daa444011e161f501683a3
SoFiA Admin <admin@SoFiA.astro>
2014-05-30 09:24:01 +0000

    Improved readoptions.py This change improves the handling of SoFiA parameter files in readoptions.py. To begin with, I added a list specifying the data types of all parameters such that the correct conversion is carried out irrespective of the actual parameter value. E.g., a '1234' in the input file name field will now be correctly converted into a string rather than an integer value. Secondly, I cleaned up the code a little bit by adding/updating comments, diverting error messages to stderr rather than stdout, and improving little bits and pieces throughout the code.

--- sofia/readoptions.py
+++ sofia/readoptions.py
@@ -4,11 +4,6 @@ import sys
 import traceback
 import ast
 
-def eprint():
-    print sys.exc_info()[1]
-    print traceback.print_tb(sys.exc_info()[2])
-
-    
 def is_number(s):
     try:
         float(s)
@@ -17,17 +12,24 @@ def is_number(s):
         return False
 
 
+def str2bool(s):
+    if s in ['True', 'true', 'TRUE', 'Yes', 'yes', 'YES']:
+        return True
+    return False
+
+
 def readPipelineOptions(filename = "pipeline.options"):
     f = open(filename, 'r')
     lines = f.readlines()
     f.close()
-    #whiteSpaceRemover = re.compile(r'\s+')
-    #lines=[whiteSpaceRemover.sub('', line) for line in lines]
     
-    lines = [line.strip(" \t\r\n") for line in lines]
+    # Remove leading/trailing whitespace and empty lines:
+    lines = [line.strip() for line in lines]
     lines = [line for line in lines if len(line) > 0]
     lines2 = []
     
+    # The following piece of code if meant to allow line breaks in parameter settings 
+    # indicated by a double backwards slash (\\):
     for l in lines:
         if len(lines2) > 0:
             if lines2[-1][-1] == "\\":
@@ -37,68 +39,126 @@ def readPipelineOptions(filename = "pipeline.options"):
     lines = lines2
     tasks = {}
     
+    # Define data types of individual parameters. This is used to 
+    # convert the input values into the correct data type.
+    # Ensure that all new parameters get added to this list!
+    datatypes = {"steps.doFlag": "bool", \
+                 "steps.doSmooth": "bool", \
+                 "steps.doScaleNoise": "bool", \
+                 "steps.doSCfind": "bool", \
+                 "steps.doThreshold": "bool", \
+                 "steps.doWavelet": "bool", \
+                 "steps.doCNHI": "bool", \
+                 "steps.doMerge": "bool", \
+                 "steps.doReliability": "bool", \
+                 "steps.doParameterise": "bool", \
+                 "steps.doWriteMask": "bool", \
+                 "steps.doWriteCat": "bool", \
+                 "steps.doMom0": "bool", \
+                 "steps.doMom1": "bool", \
+                 "steps.doCubelets": "bool", \
+                 "steps.doDebug": "bool", \
+                 "import.inFile": "string", \
+                 "import.weightsFile": "string", \
+                 "import.maskFile": "string", \
+                 "import.weightsFunction": "string", \
+                 "flag.regions": "string", \
+                 "smooth.kernel": "string", \
+                 "smooth.edgeMode": "string", \
+                 "smooth.kernelX": "float", \
+                 "smooth.kernelY": "float", \
+                 "smooth.kernelZ": "float", \
+                 "scaleNoise.statistic": "string", \
+                 "scaleNoise.edgeX": "int", \
+                 "scaleNoise.edgeY": "int", \
+                 "scaleNoise.edgeZ": "int", \
+                 "SCfind.threshold": "float", \
+                 "SCfind.sizeFilter": "float", \
+                 "SCfind.maskScaleXY": "float", \
+                 "SCfind.maskScaleZ": "float", \
+                 "SCfind.edgeMode": "string", \
+                 "SCfind.rmsMode": "string", \
+                 "SCfind.kernels": "array", \
+                 "SCfind.kernelUnit": "string", \
+                 "SCfind.verbose": "bool", \
+                 "threshold.threshold": "float", \
+                 "threshold.clipMethod": "string", \
+                 "threshold.rmsMode": "string", \
+                 "threshold.verbose": "bool", \
+                 "merge.mergeX": "int", \
+                 "merge.mergeY": "int", \
+                 "merge.mergeZ": "int", \
+                 "merge.minSizeX": "int", \
+                 "merge.minSizeY": "int", \
+                 "merge.minSizeZ": "int", \
+                 "reliability.parSpace": "array", \
+                 "reliability.kernel": "array", \
+                 "reliability.fMin": "float", \
+                 "reliability.relThresh": "float", \
+                 "parameters.fitBusyFunction": "bool", \
+                 "parameters.optimiseMask": "bool", \
+                 "writeCat.basename": "string", \
+                 "writeCat.writeASCII": "bool", \
+                 "writeCat.writeXML": "bool", \
+                 "writeCat.writeSQL": "bool", \
+                 "writeCat.parameters": "array"}
+    
+    # Loop through all lines:
     for linenr,line in enumerate(lines):
         if line[0] == "#":
             continue
+        
         try:
-            if line.count('=') != 1:
-                print "No or too many \'=\' present in line %02d: %s"%(linenr + 1, line)
+            parameter,value = tuple(line.split("=", 1))
+            if len(parameter) < 1:
+                sys.stderr.write("WARNING: Option name missing in line %02d: %s\n"%(linenr + 1, line))
                 continue
-            remainingkeys,value = tuple(line.split("="))
-            if len(remainingkeys) < 1:
-                print "Option name missing in line %02d: %s"%(linenr + 1, line)
-                continue
-            #if len(value)<1:
-                #print "value missing in            line %02d: %s"%(linenr+1,line)
-                #continue
+            
             subtasks = tasks
             
             while True:
-                keyname = remainingkeys.split(".")[0]
-                keyname = keyname.strip(" \t\r\n")
-                if len(keyname) < 1:
-                    print "(Sub)key name to short in line %02d: %s"%(linenr + 1, line)
+                module = parameter.split(".")[0]
+                module = module.strip()
+                
+                if len(module) < 1:
+                    sys.stderr.write("WARNING: (Sub)key name too short in line %02d: %s\n"%(linenr + 1, line))
                     break
                 
-                remainingkeys = str(".").join(remainingkeys.split(".")[1:])
-                remainingkeys = remainingkeys.strip(" \t\r\n")
+                parameter = str(".").join(parameter.split(".")[1:])
+                parameter = parameter.strip()
                 
-                if not subtasks.has_key(keyname):
-                    subtasks[keyname] = {}
-                subtasks=subtasks[keyname]
+                if not subtasks.has_key(module):
+                    subtasks[module] = {}
+                subtasks = subtasks[module]
                 
-                if remainingkeys.count(".") > 0:
-                    pass
-                else:
-                    if subtasks.has_key(remainingkeys):
-                        print "Option already present in line %02d: %s"%(linenr + 1, line)
+                if parameter.count(".") == 0:
+                    if subtasks.has_key(parameter):
+                        sys.stderr.write("WARNING: Option already present in line %02d: %s\n"%(linenr + 1, line))
                         break
                     try:
-                        value = value.split('#')[0].strip(" \t\r\n")
-                        if len(value) > 0 and (is_number(value) or value[0] == '['):
-                            tmpval = ast.literal_eval(value)
-                        elif value in ['True','true','TRUE','Yes','yes','YES']:
-                            tmpval = True
-                        elif value in ['False','false','FALSE','No','no','NO']:
-                            tmpval = False
+                        value = value.split('#')[0].strip()
+                        searchKey = module + "." + parameter
+                        
+                        if searchKey in datatypes:
+                            if datatypes[searchKey] == "bool":
+                                subtasks[parameter] = str2bool(value)
+                            elif datatypes[searchKey] == "float":
+                                subtasks[parameter] = float(value)
+                            elif datatypes[searchKey] == "int":
+                                subtasks[parameter] = int(value)
+                            elif datatypes[searchKey] == "array":
+                                subtasks[parameter] = ast.literal_eval(value)
+                            else:
+                                subtasks[parameter] = str(value)
                         else:
-                            tmpval = str(value)
+                            sys.stderr.write("WARNING: No data type defined for parameter %s.\n"%searchKey)
+                            sys.stderr.write("         Guessing type based on value.\n")
+                            subtasks[parameter] = ast.literal_eval(value)
                     except:
-                        print "Could not parse option in line %02d: %s"%(linenr + 1, line)
+                        sys.stderr.write("WARNING: Could not parse option in line %02d: %s\n"%(linenr + 1, line))
                         break
-                    
-                    subtasks[remainingkeys] = tmpval
                     break
         except:
-            #eprint()
             pass
-            #print line
+    
     return tasks
-#
-
-#tasks=readPipelineOptions()
-#print tasks
-
-
-
-
